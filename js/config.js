78dcb5938a9a7874c5cd5a80d302f3594935722f
AMBDY <109236757+AMBDY@users.noreply.github.com>
2026-05-08 12:06:24 +0100

    config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,8 @@
 // Automatically detects if you are testing on your laptop or if the site is live.
-const API_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+const API_URL =  https://refai-backend-l1bp.onrender.com
+window.location.hostname === '127.0.0.1'
     ? 'http://localhost:5000' 
-    : 'https://api.refai.com.ng'; // Replace with your actual live backend URL later
+    : 'https://refai-backend-l1bp.onrender.com'; // Replace with your actual live backend URL later
 
 window.API_URL = API_URL;
 console.log('🔧 RefAI Engine Connected to:', API_URL);
